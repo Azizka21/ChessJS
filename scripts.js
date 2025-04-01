678eff092f0964787c5da89dd381f32e877f5101
Azizka21 <sudoheckpentagon@gmail.com>
2025-04-01 18:02:00 +0500

    Pieces object and startposition

--- scripts.js
+++ scripts.js
@@ -3,15 +3,16 @@ window.onload = function() {
 }
 
 class Piece {
-    constructor(color) {
-        this.color = color
+    constructor(pieceType, color) {
+        this.pieceType = pieceType;
+        this.color = color;
     }
 }
 
 class Cell {
-    constructor(row, column, piece = null) {
-        this.position = this.getPosition(row,column)
-        this.piece = piece;
+    constructor(row, column, piece) {
+        this.position = this.getPosition(row,column);
+        this.piece = new Piece(piece[0], piece[1]);
     }
     getPosition(row, column) {
         const numToLetter = {
@@ -29,21 +30,56 @@ class Cell {
 }
 
 class Row {
-    constructor(rowIndex) {
+    constructor(rowIndex, rowStatement) {
         this.cells = []
         this.index = rowIndex
         for (let i=1; i<=8; i++) {
-            this.cells.push(new Cell(this.index, i));
+            this.cells.push(new Cell(this.index, i, rowStatement[i-1]));
         }
     }
 }
 
 class Board {
-    constructor() {
+    constructor(statement = startposition) {
         this.rows = []
-        for (let i=1; i<=8; i++) {
-            this.rows.push(new Row(i));
+        for (let i = 1; i <= 8; i++) {
+            this.rows.push(new Row(i, statement[i - 1]));
         }
         console.dir(this)
     }
 }
+
+const startposition = [
+    [
+    ["Rook", "white"], ["Knight", "white"], ["Bishop", "white"], ["Queen", "white"],
+    ["King", "white"], ["Bishop", "white"], ["Knight", "white"], ["Rook", "white"]
+    ],
+    [
+    ["Pawn", "white"], ["Pawn", "white"], ["Pawn", "white"], ["Pawn", "white"],
+    ["Pawn", "white"], ["Pawn", "white"], ["Pawn", "white"], ["Pawn", "white"]
+    ],
+    [
+    [null, null], [null, null], [null, null], [null, null],
+    [null, null], [null, null], [null, null], [null, null],
+    ],
+    [
+    [null, null], [null, null], [null, null], [null, null],
+    [null, null], [null, null], [null, null], [null, null],
+    ],
+    [
+    [null, null], [null, null], [null, null], [null, null],
+    [null, null], [null, null], [null, null], [null, null],
+    ],
+    [
+    [null, null], [null, null], [null, null], [null, null],
+    [null, null], [null, null], [null, null], [null, null],
+    ],
+    [
+    ["Pawn", "black"], ["Pawn", "black"], ["Pawn", "black"], ["Pawn", "black"],
+    ["Pawn", "black"], ["Pawn", "black"], ["Pawn", "black"], ["Pawn", "black"]
+    ],
+    [
+    ["Rook", "black"], ["Knight", "black"], ["Bishop", "black"], ["Queen", "black"],
+    ["King", "black"], ["Bishop", "black"], ["Knight", "black"], ["Rook", "black"]
+    ]
+];
